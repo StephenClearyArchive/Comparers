b67508cc0a7f9a6b025f2061361d455063edb56b
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-19 16:45:59 -0400

    Changed creation APIs for comparers.

---
+++ Source/Comparers/Compare.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace Comparers
+{
+    /// <summary>
+    /// Provides sources for comparers.
+    /// </summary>
+    public static class Compare
+    {
+        /// <summary>
+        /// Gets the null comparer for this type, which evaluates all objects as equivalent.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        public static IComparer<T> Null<T>()
+        {
+            Contract.Ensures(Contract.Result<IComparer<T>>() != null);
+            return NullComparer<T>.Instance;
+        }
+
+        /// <summary>
+        /// Gets the default comparer for this type.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        public static IComparer<T> Default<T>()
+        {
+            Contract.Ensures(Contract.Result<IComparer<T>>() != null);
+            return DefaultComparer<T>.Instance;
+        }
+
+        /// <summary>
+        /// Provides a method for creating a key comparer.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        public static class Key<T>
+        {
+            /// <summary>
+            /// Creates a key comparer.
+            /// </summary>
+            /// <typeparam name="TKey">The type of key objects being compared.</typeparam>
+            /// <param name="selector">The key selector.</param>
+            /// <param name="keyComparer">The key comparer.</param>
+            /// <returns>A key comparer.</returns>
+            public static IComparer<T> OrderBy<TKey>(Func<T, TKey> selector, IComparer<TKey> keyComparer = null)
+            {
+                Contract.Requires(selector != null);
+                Contract.Ensures(Contract.Result<IComparer<T>>() != null);
+                return Null<T>().ThenBy(selector, keyComparer);
+            }
+        }
+    }
+}
