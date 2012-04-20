46a062d192aab75e182c181678538de7369f5e39
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-19 21:00:17 -0400

    Corrected AssemblyInfo.

--- Source/Comparers/Properties/AssemblyInfo.cs
+++ Source/Comparers/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Comparers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microline Technology Corporation")]
-[assembly: AssemblyProduct("Comparers")]
-[assembly: AssemblyCopyright("Copyright © Microline Technology Corporation 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Comparers Library for .NET")]
+[assembly: AssemblyDescription("IComparer<T> implementations and extension methods.")]
+[assembly: AssemblyCompany("Nito Programs")]
+[assembly: AssemblyProduct("Nito.Comparers")]
+[assembly: AssemblyCopyright("Copyright © 2009-2012 Nito Programs")]
+[assembly: CLSCompliant(true)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("492b4702-6c47-481f-9cb7-8be03c81295c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
