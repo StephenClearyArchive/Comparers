33beb79bb0790cee38a4c7d8157af7aa07254a69
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-20 19:29:03 -0400

    Changed the NuGet build script to a solution/NuGet build script.

--- Source/Build.cmd
+++ Source/Build.cmd
@@ -1,5 +1,6 @@
 @echo off
 if not exist ..\Binaries mkdir ..\Binaries
 if not exist ..\Binaries\NuGet mkdir ..\Binaries\NuGet
+devenv Comparers.sln /rebuild Release
 ..\Util\nuget.exe pack -sym Comparers.nuspec -o ..\Binaries\NuGet
 pause
