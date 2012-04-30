f86a634a0927876cf6774614e2d5e2c24cb74835
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-30 11:41:26 -0400

    Added operator overloading and ComparableBaseWithOperators.

--- Source/Comparers/Util/ComparableImplementations.cs
+++ Source/Comparers/Util/ComparableImplementations.cs
@@ -21,6 +21,7 @@ namespace Comparers.Util
         public static int ImplementCompareTo<T>(IComparer<T> comparer, T @this, T other) where T : IComparable<T>
         {
             Contract.Requires(comparer != null);
+            Contract.Requires(@this != null);
             return comparer.Compare(@this, other);
         }
 
@@ -46,6 +47,7 @@ namespace Comparers.Util
         public static int ImplementGetHashCode<T>(IEqualityComparer<T> equalityComparer, T @this)
         {
             Contract.Requires(equalityComparer != null);
+            Contract.Requires(@this != null);
             return equalityComparer.GetHashCode(@this);
         }
 
@@ -59,6 +61,7 @@ namespace Comparers.Util
         public static bool ImplementEquals<T>(IEqualityComparer<T> equalityComparer, T @this, T other) where T : IEquatable<T>
         {
             Contract.Requires(equalityComparer != null);
+            Contract.Requires(@this != null);
             return equalityComparer.Equals(@this, other);
         }
 
@@ -73,6 +76,84 @@ namespace Comparers.Util
             Contract.Requires(equalityComparer != null);
             Contract.Requires(@this != null);
             return equalityComparer.Equals(@this, obj);
+        }
+
+        /// <summary>
+        /// Implements <c>op_Eqality</c>. Types overloading <c>op_Equality</c> should also overload <c>op_Inequality</c> and override <see cref="Object.Equals(Object)"/> and <see cref="Object.GetHashCode"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="equalityComparer">The comparer.</param>
+        /// <param name="left">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        /// <param name="right">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        public static bool ImplementOpEquality<T>(IEqualityComparer<T> equalityComparer, T left, T right)
+        {
+            Contract.Requires(equalityComparer != null);
+            return equalityComparer.Equals(left, right);
+        }
+
+        /// <summary>
+        /// Implements <c>op_Ineqality</c>. Types overloading <c>op_Inequality</c> should also overload <c>op_Equality</c> and override <see cref="Object.Equals(Object)"/> and <see cref="Object.GetHashCode"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="equalityComparer">The comparer.</param>
+        /// <param name="left">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        /// <param name="right">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        public static bool ImplementOpInequality<T>(IEqualityComparer<T> equalityComparer, T left, T right)
+        {
+            Contract.Requires(equalityComparer != null);
+            return !equalityComparer.Equals(left, right);
+        }
+
+        /// <summary>
+        /// Implements <c>op_LessThan</c>. Types overloading <c>op_LessThan</c> should also overload <c>op_Equality</c>, <c>op_Inequality</c>, <c>op_LessThanOrEqual</c>, <c>op_GreaterThan</c>, and <c>op_GreaterThanOrEqual</c>; and override <see cref="Object.Equals(Object)"/> and <see cref="Object.GetHashCode"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="comparer">The comparer.</param>
+        /// <param name="left">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        /// <param name="right">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        public static bool ImplementOpLessThan<T>(IComparer<T> comparer, T left, T right)
+        {
+            Contract.Requires(comparer != null);
+            return comparer.Compare(left, right) < 0;
+        }
+
+        /// <summary>
+        /// Implements <c>op_GreaterThan</c>. Types overloading <c>op_LessThan</c> should also overload <c>op_Equality</c>, <c>op_Inequality</c>, <c>op_LessThanOrEqual</c>, <c>op_LessThan</c>, and <c>op_GreaterThanOrEqual</c>; and override <see cref="Object.Equals(Object)"/> and <see cref="Object.GetHashCode"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="comparer">The comparer.</param>
+        /// <param name="left">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        /// <param name="right">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        public static bool ImplementOpGreaterThan<T>(IComparer<T> comparer, T left, T right)
+        {
+            Contract.Requires(comparer != null);
+            return comparer.Compare(left, right) > 0;
+        }
+
+        /// <summary>
+        /// Implements <c>op_LessThanOrEqual</c>. Types overloading <c>op_LessThan</c> should also overload <c>op_Equality</c>, <c>op_Inequality</c>, <c>op_LessThan</c>, <c>op_GreaterThan</c>, and <c>op_GreaterThanOrEqual</c>; and override <see cref="Object.Equals(Object)"/> and <see cref="Object.GetHashCode"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="comparer">The comparer.</param>
+        /// <param name="left">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        /// <param name="right">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        public static bool ImplementOpLessThanOrEqual<T>(IComparer<T> comparer, T left, T right)
+        {
+            Contract.Requires(comparer != null);
+            return comparer.Compare(left, right) <= 0;
+        }
+
+        /// <summary>
+        /// Implements <c>op_GreaterThanOrEqual</c>. Types overloading <c>op_LessThan</c> should also overload <c>op_Equality</c>, <c>op_Inequality</c>, <c>op_LessThan</c>, <c>op_GreaterThan</c>, and <c>op_LessThanOrEqual</c>; and override <see cref="Object.Equals(Object)"/> and <see cref="Object.GetHashCode"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="comparer">The comparer.</param>
+        /// <param name="left">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        /// <param name="right">A value of type <typeparamref name="T"/> or <c>null</c>.</param>
+        public static bool ImplementOpGreaterThanOrEqual<T>(IComparer<T> comparer, T left, T right)
+        {
+            Contract.Requires(comparer != null);
+            return comparer.Compare(left, right) >= 0;
         }
     }
 }
