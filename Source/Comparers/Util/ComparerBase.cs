8a13859a7434d8fa3040f8e6c8b78aef02aad668
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-30 11:41:00 -0400

    Fixed some border cases re null objects being compared/hashed.

--- Source/Comparers/Util/ComparerBase.cs
+++ Source/Comparers/Util/ComparerBase.cs
@@ -15,15 +15,15 @@ namespace Comparers.Util
         /// <summary>
         /// Returns a hash code for the specified object.
         /// </summary>
-        /// <param name="obj">The object for which to return a hash code.</param>
+        /// <param name="obj">The object for which to return a hash code. This object is not <c>null</c>.</param>
         /// <returns>A hash code for the specified object.</returns>
         protected abstract int DoGetHashCode(T obj);
 
         /// <summary>
         /// Compares two objects and returns a value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.
         /// </summary>
-        /// <param name="x">The first object to compare.</param>
-        /// <param name="y">The second object to compare.</param>
+        /// <param name="x">The first object to compare. This object is not <c>null</c>.</param>
+        /// <param name="y">The second object to compare. This object is not <c>null</c>.</param>
         /// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
         protected abstract int DoCompare(T x, T y);
 
@@ -49,6 +49,8 @@ namespace Comparers.Util
         /// <returns>A hash code for the specified object.</returns>
         int System.Collections.IEqualityComparer.GetHashCode(object obj)
         {
+            if (obj == null)
+                return 0;
             Contract.Assume(obj is T);
             return (this as IEqualityComparer<T>).GetHashCode((T)obj);
         }
@@ -95,6 +97,8 @@ namespace Comparers.Util
         /// <returns>A hash code for the specified object.</returns>
         public int GetHashCode(T obj)
         {
+            if (obj == null)
+                return 0;
             return this.DoGetHashCode(obj);
         }
 
@@ -106,6 +110,17 @@ namespace Comparers.Util
         /// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
         public int Compare(T x, T y)
         {
+            if (x == null)
+            {
+                if (y == null)
+                    return 0;
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+
             return this.DoCompare(x, y);
         }
     }
