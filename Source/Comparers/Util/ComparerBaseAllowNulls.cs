eb4b5c32c390223cb2a628906d72bc756bae21a4
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-04-30 13:53:36 -0400

    Added ComparerBaseAllowNulls and changed NullComparer to treat all values (including nulls) as equivalent.

--- Source/Comparers/Util/ComparerBaseAllowNulls.cs
+++ Source/Comparers/Util/ComparerBaseAllowNulls.cs
@@ -7,23 +7,23 @@ using System.Diagnostics.Contracts;
 namespace Comparers.Util
 {
     /// <summary>
-    /// Common implementations for comparers that treat <c>null</c> values as less than non-<c>null</c> values.
+    /// Common implementations for comparers that can handle <c>null</c> values.
     /// </summary>
     /// <typeparam name="T">The type of objects being compared.</typeparam>
-    public abstract class ComparerBase<T> : IFullComparer<T>
+    public abstract class ComparerBaseAllowNulls<T> : IFullComparer<T>
     {
         /// <summary>
         /// Returns a hash code for the specified object.
         /// </summary>
-        /// <param name="obj">The object for which to return a hash code. This object is not <c>null</c>.</param>
+        /// <param name="obj">The object for which to return a hash code. This object may be <c>null</c>.</param>
         /// <returns>A hash code for the specified object.</returns>
         protected abstract int DoGetHashCode(T obj);
 
         /// <summary>
         /// Compares two objects and returns a value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.
         /// </summary>
-        /// <param name="x">The first object to compare. This object is not <c>null</c>.</param>
-        /// <param name="y">The second object to compare. This object is not <c>null</c>.</param>
+        /// <param name="x">The first object to compare. This object may be <c>null</c>.</param>
+        /// <param name="y">The second object to compare. This object may be <c>null</c>.</param>
         /// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
         protected abstract int DoCompare(T x, T y);
 
@@ -35,10 +35,8 @@ namespace Comparers.Util
         /// <returns><c>true</c> if <paramref name="x"/> is equal to <paramref name="y"/>; otherwise <c>false</c>.</returns>
         bool System.Collections.IEqualityComparer.Equals(object x, object y)
         {
-            if (x == null || y == null)
-                return x == y;
-            Contract.Assume(x is T);
-            Contract.Assume(y is T);
+            Contract.Assume(x == null || x is T);
+            Contract.Assume(y == null || y is T);
             return (this as IComparer<T>).Compare((T)x, (T)y) == 0;
         }
 
@@ -49,9 +47,7 @@ namespace Comparers.Util
         /// <returns>A hash code for the specified object.</returns>
         int System.Collections.IEqualityComparer.GetHashCode(object obj)
         {
-            if (obj == null)
-                return 0;
-            Contract.Assume(obj is T);
+            Contract.Assume(obj == null || obj is T);
             return (this as IEqualityComparer<T>).GetHashCode((T)obj);
         }
 
@@ -63,19 +59,8 @@ namespace Comparers.Util
         /// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
         int System.Collections.IComparer.Compare(object x, object y)
         {
-            if (x == null)
-            {
-                if (y == null)
-                    return 0;
-                return -1;
-            }
-            else if (y == null)
-            {
-                return 1;
-            }
-
-            Contract.Assume(x is T);
-            Contract.Assume(y is T);
+            Contract.Assume(x == null || x is T);
+            Contract.Assume(y == null || y is T);
             return (this as IComparer<T>).Compare((T)x, (T)y);
         }
 
@@ -97,8 +82,6 @@ namespace Comparers.Util
         /// <returns>A hash code for the specified object.</returns>
         public int GetHashCode(T obj)
         {
-            if (obj == null)
-                return 0;
             return this.DoGetHashCode(obj);
         }
 
@@ -110,17 +93,6 @@ namespace Comparers.Util
         /// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
         public int Compare(T x, T y)
         {
-            if (x == null)
-            {
-                if (y == null)
-                    return 0;
-                return -1;
-            }
-            else if (y == null)
-            {
-                return 1;
-            }
-
             return this.DoCompare(x, y);
         }
     }
--- Source/Comparers/Util/NullComparer.cs
+++ Source/Comparers/Util/NullComparer.cs
@@ -10,7 +10,7 @@ namespace Comparers.Util
     /// The null comparer.
     /// </summary>
     /// <typeparam name="T">The type of objects being compared.</typeparam>
-    public sealed class NullComparer<T> : ComparerBase<T>
+    public sealed class NullComparer<T> : ComparerBaseAllowNulls<T>
     {
         private NullComparer()
         {
@@ -20,27 +20,6 @@ namespace Comparers.Util
         static NullComparer()
         {
             // This type constructor does nothing; it only exists to make static field initialization deterministic.
-        }
-
-        /// <summary>
-        /// Returns a hash code for the specified object.
-        /// </summary>
-        /// <param name="obj">The object for which to return a hash code.</param>
-        /// <returns>A hash code for the specified object.</returns>
-        protected override int DoGetHashCode(T obj)
-        {
-            return 0;
-        }
-
-        /// <summary>
-        /// Compares two objects and returns a value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.
-        /// </summary>
-        /// <param name="x">The first object to compare.</param>
-        /// <param name="y">The second object to compare.</param>
-        /// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
-        protected override int DoCompare(T x, T y)
-        {
-            return 0;
         }
 
         private static readonly NullComparer<T> instance = new NullComparer<T>();
@@ -55,6 +34,27 @@ namespace Comparers.Util
                 Contract.Ensures(Contract.Result<NullComparer<T>>() != null);
                 return instance;
             }
+        }
+
+        /// <summary>
+        /// Returns a hash code for the specified object.
+        /// </summary>
+        /// <param name="obj">The object for which to return a hash code. This object may be <c>null</c>.</param>
+        /// <returns>A hash code for the specified object.</returns>
+        protected override int DoGetHashCode(T obj)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Compares two objects and returns a value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.
+        /// </summary>
+        /// <param name="x">The first object to compare. This object may be <c>null</c>.</param>
+        /// <param name="y">The second object to compare. This object may be <c>null</c>.</param>
+        /// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>, 0 if <paramref name="x"/> is equal to <paramref name="y"/>, or greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
+        protected override int DoCompare(T x, T y)
+        {
+            return 0;
         }
 
         /// <summary>
