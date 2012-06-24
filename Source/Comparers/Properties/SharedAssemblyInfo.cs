41b73abf56fa9f7f6556e7a3a72687254a75ff5c
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-06-23 21:43:07 -0400

    Personlized copyright and updated dll description.

--- Source/Comparers/Properties/SharedAssemblyInfo.cs
+++ Source/Comparers/Properties/SharedAssemblyInfo.cs
@@ -2,8 +2,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("IComparer<T> implementations and extension methods.")]
-[assembly: AssemblyCompany("Nito Programs")]
-[assembly: AssemblyProduct("Nito.Comparers")]
-[assembly: AssemblyCopyright("Copyright © 2009-2012 Nito Programs")]
+[assembly: AssemblyDescription("IComparer<T> and IEqualityComparer<T> implementations, extension methods, and comparable/equatable helper types.")]
+[assembly: AssemblyCompany("Stephen Cleary")]
+[assembly: AssemblyCopyright("Copyright © 2009-2012 Stephen Cleary")]
 [assembly: CLSCompliant(true)]
