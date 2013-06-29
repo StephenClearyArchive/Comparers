27f87343b6fe42b6a652ac4f8e634e76e3de8135
Stephen Cleary <StephenCleary@users.noreply.github.com>
2013-06-29 16:27:50 -0400

    Finished support for anonymous types.

---
+++ Source/Comparers/Comparers/Compare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+using Comparers.Util;
+
+namespace Comparers
+{
+    /// <summary>
+    /// Provides sources for comparers.
+    /// </summary>
+    public static class Compare
+    {
+        /// <summary>
+        /// Gets a comparer source for type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="_">Parameter that is only used to derive the type <typeparamref name="T"/>.</param>
+        public static CompareSource<T> Type<T>(T _ = default(T))
+        {
+            return new CompareSource<T>();
+        }
+    }
+}
---
+++ Source/Comparers/EqualityComparers/EqualityCompare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+using Comparers;
+using EqualityComparers.Util;
+
+namespace EqualityComparers
+{
+    /// <summary>
+    /// Provides sources for equality comparers.
+    /// </summary>
+    public static class EqualityCompare
+    {
+        /// <summary>
+        /// Gets an equality comparer source for type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects being compared.</typeparam>
+        /// <param name="_">Parameter that is only used to derive the type <typeparamref name="T"/>.</param>
+        public static EqualityCompareSource<T> Type<T>(T _ = default(T))
+        {
+            return new EqualityCompareSource<T>();
+        }
+    }
+}
