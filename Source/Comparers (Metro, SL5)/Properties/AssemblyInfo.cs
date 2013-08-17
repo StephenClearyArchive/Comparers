042a01527d22bbd768e87c00a2451915599a1d65
Stephen Cleary <StephenCleary@users.noreply.github.com>
2013-08-16 20:25:14 -0400

    Added PCL support.

--- Source/Comparers (Metro, SL5)/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Comparers Library for Metro and Silverlight 5 (also supports .NET 4.0)")]
--- Source/Comparers (SL4, WP7, XBox)/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Comparers Library for Silverlight 4, Windows Phone 7, and XBox (also supports .NET 4.0 and Metro)")]
